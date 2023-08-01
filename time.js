002b2e0dee922ca528a19ec067d66b501595549b
LaProfessional <118113982+LaProfessional@users.noreply.github.com>
2023-08-01 23:49:49 +0700

    Windows-10

--- time.js
+++ time.js
@@ -1,34 +1,41 @@
-const dateElement = document.querySelector('.taskbar-date');
-const timeElement = document.querySelector('.taskbar-time');
-
-const formatDateTimePart = (number) => {
-    return String(number).padStart(2, '0');
-};
-
-const updateDate = (date) => {
-    const day = formatDateTimePart(date.getDate());
-    const month = formatDateTimePart(date.getMonth() + 1);
-    const year = String(date.getFullYear());
-
-    dateElement.textContent = `${day}.${month}.${year}`;
-};
-
-const updateTime = (date) => {
-    const hours = formatDateTimePart(date.getHours());
-    const minutes = formatDateTimePart(date.getMinutes());
-    const seconds = formatDateTimePart(date.getSeconds());
-
-    timeElement.textContent = `${hours}:${minutes}:${seconds}`;
-};
-
-const updateDateTime = () => {
-    const date = new Date();
-
-    updateTime(date);
-    updateDate(date);
-};
-
-updateDateTime();
 setInterval(function () {
-    updateDateTime();
-}, 1000);
+    const time = document.querySelector('.taskbar-time');
+    const dateTime = new Date();
+
+    let hours = dateTime.getHours();
+    let minutes = dateTime.getMinutes();
+    let seconds = dateTime.getSeconds();
+
+    if (hours < 10) {
+        hours = hours - 10;
+    }
+    if (hours < 10) {
+        hours = '0' + hours;
+    }
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+    if (seconds < 10) {
+        seconds = '0' + seconds
+    }
+
+    time.textContent = hours + ':' + minutes + ':' + seconds;
+})
+
+setInterval(function () {
+    const date = document.querySelector('.taskbar-date');
+    const dateTime = new Date();
+
+    let days = dateTime.getDate();
+    let months = dateTime.getMonth() + 1;
+    const years = dateTime.getFullYear();
+
+    if (days < 10) {
+        days = '0' + days;
+    }
+    if (months < 10) {
+        months = '0' + months;
+    }
+
+    date.textContent = days + '.' + months + '.' + years;
+})
